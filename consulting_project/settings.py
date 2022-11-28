d589fdba6ab6d54fd31cf0d7d6b1ddbfcd8cc89a
meder <meder@example.com>
2022-11-28 13:10:07 +0600

    seccond commit

--- consulting_project/settings.py
+++ consulting_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ggo!2i86an4sd093_emh8e!i0@igmadtvs#p5uj6g83-dz@34'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'consalting.herokuapp.com']
 
 
 # Application definition
